fabdadc96e2bb279d44cbd69df1a62526fa79874
Robert Sosinski <robert.sosinski@reactive.io>
2015-09-23 20:12:20 +0000

    validation and Federal Waste Codes tagging

--- _static/assets/js/app.js
+++ _static/assets/js/app.js
@@ -8,4 +8,18 @@
 
     self.name = 'Hello World!!';
   }]);
+
+
+  $(function() {
+    $('#manifest_item_epa_waste_code_1').selectize({
+      delimiter: ',',
+      // persist: false,
+      create: function(input) {
+          return {
+              value: input,
+              text: input
+          };
+      }
+    });
+  });
 })();
