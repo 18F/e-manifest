2eb4ba612e29a74a41cb0472e1450a84838333df
Brandon Kirby <brandon.kirby@gsa.gov>
2015-10-07 13:20:27 -0400

    Force 10-digit phone numbers (do not allow 7-digit local calls).
    Reformat the phone number when it is valid to match the nnn-nnn-nnnn
    format.
    
    If the number is valid and then is made invalid, Angular is still
    calling the ng-change handler. Annoying. We really want to pass a
    reference to the model we want to examine so our "onBlur" handler
    can reformat and set the value only when the input/model is valid.
    
    This likely needs a custom directive.
    
    Ignoring the error, this does what we want it to do...

--- _static/assets/js/app.js
+++ _static/assets/js/app.js
@@ -66,8 +66,20 @@
         window.location.href = '/web/done.html';
       });
     };
+
+    $scope.formatCanonicalPhone = function(phoneNumber) {
+      var phoneDigits = phoneNumber.replace(/\D/g, "");
+      var containsPlusOnePrefix = (phoneDigits.length === 11);
+
+      if (containsPlusOnePrefix) {
+        phoneDigits = phoneDigits.substr(1);
+      }
+
+      var canonicalPhone = phoneDigits.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+      return canonicalPhone;
+    };
     
-    $scope.phonePattern = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?\(?\s*(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*\)?\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$/;
+    $scope.phonePattern = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?\(?\s*(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*\)?\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$/;
   }]);
 
     
