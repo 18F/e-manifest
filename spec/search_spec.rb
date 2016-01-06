d57c578eedfc3cd088761ad44e9fb63b8e8be8fe
Peter Karman <peter.karman@gsa.gov>
2016-01-06 08:22:19 -0600

    populate_manifests spec helper takes optional number of records to create

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -6,27 +6,27 @@ require "queryparams"
 RSpec.describe "search" do
   describe "/search" do
     it "searches all fields by default" do
-      populate_manifests
+      populate_manifests(2)
       search_for "around+the+corner"
-      expect(JSON.parse(last_response.body)["total"]).to eq 10
+      expect(JSON.parse(last_response.body)["total"]).to eq 2
     end
 
     it "searches by specific field" do
-      populate_manifests
+      populate_manifests(2)
       search_for "content.generator.name:generator"
-      expect(JSON.parse(last_response.body)["total"]).to eq 10
+      expect(JSON.parse(last_response.body)["total"]).to eq 2
     end
 
     it "searched by structured query" do
-      populate_manifests
+      populate_manifests(2)
       search_for_advanced({ "content.generator.name" => "generator" })
-      expect(JSON.parse(last_response.body)["total"]).to eq 10
+      expect(JSON.parse(last_response.body)["total"]).to eq 2
     end
   end
 
-  def populate_manifests
+  def populate_manifests(nrecords=10)
     populator = Populator.new(Manifest)
-    populator.run(10)
+    populator.run(nrecords)
     # must index manually because triggers are disabled in test env
     Manifest.rebuild_index
   end
