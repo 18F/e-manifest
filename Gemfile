61f6c890fe9652a614b522fd153c1b62a0018207
Jessie A Young <jessica.young@gsa.gov>
2016-01-06 17:18:21 -0800

    Validate against JSON schema in specs
    * Move manifest to `lib` dir
    * Use json_matchers gem to validate that specs match schema
    * https://github.com/thoughtbot/json_matchers
    * Part I of https://trello.com/c/4zYZoWWl

--- Gemfile
+++ Gemfile
@@ -19,3 +19,7 @@ group :development, :test do
   gem "rack-test", require: "rack/test"
   gem 'rspec'
 end
+
+group :test do
+  gem 'json_matchers'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,9 @@ require 'stringio'
 ENV['RACK_ENV'] = 'test'
 require_relative '../lib/app_manifest'
 
+require 'json_matchers/rspec'
+JsonMatchers.schema_root = 'lib/schemas'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
