de7fa81232cdaa00608ca70b1ef2e4dc5da5cd1d
Jessie A Young <jessica.young@gsa.gov>
2016-01-11 11:35:14 -0800

    Update specs to use v0, not v1

--- spec/requests/api/v0/post_manifest_spec.rb
+++ spec/requests/api/v0/post_manifest_spec.rb
@@ -3,12 +3,12 @@ require_relative "../../../support/request_spec_helper"
 describe 'POST manifests' do
   it 'creates a new manifest' do
     expect {
-      send_json(:post, '/api/v1/manifests?tracking_number=30221', { hello: 'world' })
+      send_json(:post, '/api/v0/manifests?tracking_number=30221', { hello: 'world' })
     }.to change { Manifest.count }.by(1)
 
     manifest = Manifest.last
 
     expect(manifest.content).to eq({ 'hello' => 'world' })
-    expect(last_response.headers['Location']).to eq("/api/v1/manifests/#{manifest.id}")
+    expect(last_response.headers['Location']).to eq("/api/v0/manifests/#{manifest.id}")
   end
 end
