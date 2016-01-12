83f35466c920c7c335ee6be23259c6abd7c9cce0
Jessie A Young <jessica.young@gsa.gov>
2016-01-12 13:58:50 -0800

    Add submissions#new path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'manifests#index'
   resources :manifests, only: [:new]
+  resources :submissions, only: [:new]
 
   namespace :api do
     namespace :v0 do
