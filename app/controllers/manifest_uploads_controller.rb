977848058676b0164b9917009590f6173d4e44eb
Jessie A Young <jessica.young@gsa.gov>
2016-01-13 16:37:45 -0800

    Extract private methods to make Code Climate happy

--- app/controllers/manifest_uploads_controller.rb
+++ app/controllers/manifest_uploads_controller.rb
@@ -3,25 +3,44 @@ class ManifestUploadsController < ApplicationController
   end
 
   def create
-    upload = params[:manifest][:upload]
-    file = File.open(upload.tempfile).read
-    encoded_file = Base64.encode64(file)
-    image_details = {
-      file_name: upload.original_filename,
-      content: encoded_file,
-      content_type: upload.content_type,
-    }
-
-    if params[:manifest_id]
-      manifest = Manifest.find(params[:manifest_id])
-      manifest.content[:image] = image_details
-    else
-      manifest = Manifest.new(content: { image: image_details })
-    end
+    manifest = find_or_initialize_manifest
 
     if manifest.save
       flash[:notice] = "Upload for manifest #{manifest.tracking_number} submitted successfully."
       redirect_to root_path
     end
+  end
+
+  private
+
+  def find_or_initialize_manifest
+    if params[:manifest_id]
+      manifest = Manifest.find(params[:manifest_id])
+      manifest.content[:image] = image_details
+      manifest
+    else
+      Manifest.new(content: { image: image_details })
+    end
+  end
+
+  def image_details
+    {
+      file_name: upload.original_filename,
+      content: encoded_file,
+      content_type: upload.content_type,
+    }
+
+  end
+
+  def upload
+    @_upload ||= params[:manifest][:upload]
+  end
+
+  def encoded_file
+    Base64.encode64(file)
+  end
+
+  def file
+    File.open(upload.tempfile).read
   end
 end
