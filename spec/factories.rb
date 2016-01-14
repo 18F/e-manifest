dd1c61ca46493c9ec439dd84f0675f6100155b62
Peter Karman <peter.karman@gsa.gov>
2016-01-13 21:03:46 -0600

    for the want of a comma the kingdom was lost

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :manifest do
     content {{
       generator: {
-        manifest_tracking_number: SecureRandom.random_number(1_000_000_000)
+        manifest_tracking_number: SecureRandom.random_number(1_000_000_000),
         name: "Company name",
       }
     }}
