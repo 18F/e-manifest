12f572f69d2d8b8751b56d8bb04fbe67b62d5d1d
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-01-14 19:30:25 -0500

    Show real data for manifest

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 require_relative '../search/query_dsl'
 
 class Manifest < ActiveRecord::Base
@@ -13,14 +15,71 @@ class Manifest < ActiveRecord::Base
     end
   end
 
+  def created_on
+    created_at.strftime("%m/%d/%Y")
+  end
+
   def disposal_facility
     if content["generator"] && content["generator"]["designated_facility"]
       content["generator"]["designated_facility"]["name"] || ""
     end
   end
 
-  def created_on
-    created_at.strftime("%m/%d/%Y")
+  def generator_emergency_response_phone
+    if content["generator"]
+      content["generator"]["emergency_response_phone"] || ""
+    end
+  end
+
+  def generator_phone_number
+    if content["generator"]
+      content["generator"]["phone_number"] || ""
+    end
+  end
+
+  def generator_mailing_address
+    if content["generator"]
+      content["generator"]["mailing_address"] || {}
+    end
+  end
+
+  def transporters
+    if content["transporters"]
+      content["transporters"] || []
+    end
+  end
+
+  def designated_facility_name
+    if content["designated_facility"]
+      content["designated_facility"]["name"] || ""
+    end
+  end
+
+  def designated_facility_signed_date
+    if content["designated_facility"]
+      y = content["designated_facility"]["certification"]["year"].to_i
+      m = content["designated_facility"]["certification"]["month"].to_i
+      d = content["designated_facility"]["certification"]["day"].to_i
+      Date.new(y, m, d)
+    end
+  end
+
+  def manifest_items
+    if content["manifest_items"]
+      content["manifest_items"] || []
+    end
+  end
+
+  def waste_handling_instructions
+    if content["waste_handling_instructions"]
+      content["waste_handling_instructions"] || ""
+    end
+  end
+
+  def waste_report_codes
+    if content["report_management_method_codes"]
+      content["report_management_method_codes"] || []
+    end
   end
 
   include Elasticsearch::Model
