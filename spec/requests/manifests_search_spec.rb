ee780aea749ae174167d98e4ef6a8756ff0057d9
Peter Karman <peter.karman@gsa.gov>
2016-01-15 21:40:02 -0600

    refactor tests to use factories instead of db populator

--- spec/requests/manifests_search_spec.rb
+++ spec/requests/manifests_search_spec.rb
@@ -1,20 +1,16 @@
 require 'rails_helper'
-require_relative '../../db/chores/populator'
 require "queryparams"
 
 describe 'manifests search', elasticsearch: true do
-  context 'ui' do
-    it 'supports structured query syntax' do
-      populate_manifests(2)
-      manifest = Manifest.last
-      get "/manifests?aq[content.generator.name]=generator"
-      expect(response.body).to include(manifest.tracking_number.to_s)
-    end
+  before(:each) do
+    Manifest.rebuild_index
   end
 
-  def populate_manifests(nrecords=10)
-    populator = Populator.new(Manifest)
-    populator.run(nrecords)
-    Manifest.rebuild_index
+  context 'ui' do
+    it 'supports structured query syntax' do
+      manifest = create(:manifest, :indexed)
+      get "/manifests?aq[content.generator.name]=company"
+      expect(response.body).to include(manifest.tracking_number.to_s)
+    end
   end
 end
