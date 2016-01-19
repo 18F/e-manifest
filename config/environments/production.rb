fd9b3f193f6e996550e06102e0185bd2f1770a25
Peter Karman <peter.karman@gsa.gov>
2016-01-19 16:15:27 -0600

    disable smtp etc on cloud.gov

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,23 +1,17 @@
-require Rails.root.join("config/smtp")
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
   config.middleware.use Rack::Deflater
-  config.middleware.use Rack::CanonicalHost, ENV.fetch("HOST")
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
-  config.log_level = :debug
-  config.action_controller.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST"))
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
+  config.log_level = :info
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 end
 Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 10).to_i
