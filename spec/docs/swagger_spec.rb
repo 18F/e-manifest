6dd711a95158ac92a9e31fba06c15456fcb00fd9
Peter Karman <peter.karman@gsa.gov>
2016-01-21 22:06:51 -0600

    lots more swagger tests

--- spec/docs/swagger_spec.rb
+++ spec/docs/swagger_spec.rb
@@ -10,14 +10,56 @@ describe 'Swagger docs', type: :apivore, order: :defined do
 
   context 'has valid paths' do
     it do
+      expect(subject).to validate(:get, '/manifests/{id}', 200, params)
+    end
+    it do
+      expect(subject).to validate(:get, '/manifests/{id}', 404, unknown_manifest_params)
+    end
+    it do
       expect(subject).to validate(
-        :get, '/manifests/{id}', 200, params
+        # TODO must we require 'manifest' key? that is inconsistent with PATCH and /validate
+        :post, '/manifests', 201, { '_data' => { manifest: manifest_as_json }.to_json }.merge(header_params)
       )
     end
     it do
       expect(subject).to validate(
-        :post, '/manifests', 201, { '_data' => { manifest: JSON.parse(manifest_as_json) }.to_json, '_headers' => set_headers }
+        :post, '/manifests', 422, { '_data' => { manifest: { invalid_json: true } }.to_json }.merge(header_params)
       )
+    end
+    it do
+      manifest.content = manifest_as_json
+      manifest.save!
+      expect(subject).to validate(
+        :patch, '/manifests/{id}', 200, params.merge(patch_params).merge(header_params)
+      )
+    end
+    it do
+      expect(subject).to validate(
+        :patch, '/manifests/{id}', 404, unknown_manifest_params.merge(patch_params).merge(header_params)
+      )
+    end
+    it do
+      expect(subject).to validate(
+        :get, '/manifests/search', 200, { '_query_string' => 'q=test' }
+      )
+    end
+    it do
+      expect(subject).to validate(
+        :get, '/manifests/search', 400, {}
+      )
+    end
+    it do
+      expect(subject).to validate(
+        :post, '/manifests/validate', 200, { '_data' => manifest_as_json.to_json }.merge(header_params)
+      )
+    end
+    it do
+      expect(subject).to validate(
+        :post, '/manifests/validate', 422, { '_data' => { invalid_json: true }.to_json }.merge(header_params)
+      )
+    end
+    it do
+      expect(subject).to validate(:get, '/method_codes', 200)
     end
   end
 
@@ -36,7 +78,27 @@ describe 'Swagger docs', type: :apivore, order: :defined do
     @_params ||= { 'id' => manifest.uuid }
   end
 
+  def unknown_manifest_params
+    { 'id' => 'foobar' }
+  end
+
+  def header_params
+    { '_headers' => set_headers }
+  end
+
+  def patch_params
+    { '_data' => manifest_patch_as_json.to_json }
+  end
+
   def manifest_as_json
-    File.read("#{Rails.root.join('app', 'views', 'examples')}/_manifest.json")
+    read_example_file_as_json('manifest')
+  end
+
+  def manifest_patch_as_json
+    read_example_file_as_json('manifest_patch')
+  end
+
+  def read_example_file_as_json(name)
+    JSON.parse(File.read("#{Rails.root.join('app', 'views', 'examples')}/_#{name}.json"))
   end
 end
