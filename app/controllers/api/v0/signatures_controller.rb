e8eec0f047578f8bc4a8ea450e18f0b22222afb2
Peter Karman <peter.karman@gsa.gov>
2016-01-24 00:55:36 -0600

    refactor, breaking one method into three

--- app/controllers/api/v0/signatures_controller.rb
+++ app/controllers/api/v0/signatures_controller.rb
@@ -1,24 +1,35 @@
 class Api::V0::SignaturesController < ApiController
   def create
     manifest = find_manifest(params[:manifest_id])
-    sign_request = read_body_as_json(symbolize_names: true)
+    signature_request = prep_signature_request(manifest)
 
-    manifest_content = manifest[:content].to_json
-    sign_request[:manifest_content] = manifest_content
+    cdx_response = CDX::Manifest.new(signature_request).sign
 
-    if sign_request[:token]
-      sign_request[:token] = Base64.strict_decode64(sign_request[:token])
+    status = update_manifest(cdx_response, signature_request, manifest)
+
+    render(json: cdx_response.to_json, status: status) unless performed?
+  end
+
+  private
+
+  def prep_signature_request(manifest)
+    signature_request = read_body_as_json(symbolize_names: true)
+    signature_request[:manifest_content] = manifest[:content].to_json
+    if signature_request[:token]
+      signature_request[:token] = Base64.strict_decode64(signature_request[:token])
     end
+    signature_request
+  end
 
-    response = CDX::Manifest.new(sign_request).sign
-
-    if (response.key?(:document_id))
-      manifest[:document_id] = response[:document_id]
-      manifest[:activity_id] = sign_request[:activity_id]
+  def update_manifest(cdx_response, signature_request, manifest)
+    if cdx_response.key?(:document_id)
+      manifest.document_id = cdx_response[:document_id]
+      manifest.activity_id = signature_request[:activity_id]
       manifest.save!
-      render(json: response.to_json, status: 200) unless performed?
+      status = 200
     else
-      render(json: response.to_json, status: 422) unless performed?
+      status = 422
     end
+    status
   end
 end
