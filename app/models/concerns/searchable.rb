27a369a137e007492312817000c7b26d45875339
Peter Karman <peter.karman@gsa.gov>
2016-01-26 09:46:17 -0600

    refactor to simplify

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -6,40 +6,28 @@ module Searchable
 
     ActiveRecord::Base.raise_in_transactional_callbacks = true
 
-    after_commit on: [:create] do
-      unless Rails.env.test?
-        reindex_async(:index)
-      end 
-    end 
-  
-    after_commit on: [:update] do
-      unless Rails.env.test?
-        reindex_async(:update)
-      end 
-    end 
-  
-    after_commit on: [:destroy] do
-      unless Rails.env.test?
-        IndexerWorker.perform_async(:delete,  self.class.to_s, self.id)
-      end 
-    end 
-  
+    after_commit on: [:create] { reindex_async(:index) }
+    after_commit on: [:update] { reindex_async(:update) }
+    after_commit on: [:destroy] { reindex_async(:delete) }
+
     def reindex
       __elasticsearch__.index_document
-    end 
-  
+    end
+
     def reindex_async(operation)
-      IndexerWorker.perform_async(operation,  self.class.to_s, self.id)
-    end 
-  
+      unless Rails.env.test?
+        IndexerWorker.perform_async(operation,  self.class.to_s, id)
+      end
+    end
+
     def remove_from_index
       __elasticsearch__.destroy_document
-    end 
-  
+    end
+
     def self.rebuild_index
       __elasticsearch__.create_index! force: true
       __elasticsearch__.import
       __elasticsearch__.refresh_index!
-    end 
+    end
   end
-end 
+end
