e2bb9e835c307cb1d689b6347ad5079522e3a541
Peter Karman <peter.karman@gsa.gov>
2016-01-26 13:09:44 -0600

    fix test text

--- spec/requests/manifests_search_spec.rb
+++ spec/requests/manifests_search_spec.rb
@@ -12,7 +12,7 @@ describe 'manifests search', elasticsearch: true do
     it 'paginates' do
       5.times { create(:manifest, :indexed) }
       get "/manifests?aq[content.generator.name]=company&page=2&size=2"
-      expect(response.body).to include("Currently viewing 3-4 of 10 results")
+      expect(response.body).to include("Currently viewing 3-4 of 5 results")
     end
   end
 end
