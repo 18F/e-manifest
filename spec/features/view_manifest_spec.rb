9f1d66a3abcd24b745f3528abaa0769a15d0f084
Jessie A Young <jessica.young@gsa.gov>
2016-01-26 13:17:54 -0800

    Allow download of scanned images
    * Show helpful error message if file upload is not added

---
+++ spec/features/view_manifest_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature 'View manifest' do
+  scenario 'manifest has a file uploaded during creation', elasticsearch: true do
+    manifest_tracking_number = '987654321abc'
+    visit new_manifest_upload_path
+    fill_in 'Manifest Tracking Number', with: manifest_tracking_number
+    attach_file('manifest[uploaded_file]', Rails.root + "spec/support/fixtures/epa_form.pdf")
+    click_on 'Submit'
+
+    visit manifest_path(manifest_tracking_number)
+
+    expect(page).to have_content('Download File Upload')
+  end
+
+  scenario 'manifest has file uploaded after creation', elasticsearch: true do
+    manifest_tracking_number = '987654321abc'
+    visit new_manifest_path
+
+    fill_in 'Manifest Tracking Number (4)', with: manifest_tracking_number
+    click_on 'Continue'
+    click_on 'Upload'
+    attach_file('manifest[uploaded_file]', Rails.root + "spec/support/fixtures/epa_form.pdf")
+    click_on 'Submit'
+
+    visit manifest_path(manifest_tracking_number)
+
+    expect(page).to have_content('Download File Upload')
+  end
+
+  scenario 'manifest does not have a file upload' do
+    manifest_tracking_number = '987654321abc'
+    visit new_manifest_path
+    fill_in 'Manifest Tracking Number (4)', with: manifest_tracking_number
+    click_on 'Continue'
+
+    visit manifest_path(manifest_tracking_number)
+
+    expect(page).not_to have_content('Download File Upload')
+    expect(page).to have_content('No scanned image available for this manifest.')
+  end
+end
