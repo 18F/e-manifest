983f28dba92022c2824fb90d72c29a4ad21cc2fe
Peter Karman <peter.karman@gsa.gov>
2016-01-26 12:30:39 -0600

    add test for search pagination

--- spec/requests/manifests_search_spec.rb
+++ spec/requests/manifests_search_spec.rb
@@ -8,5 +8,11 @@ describe 'manifests search', elasticsearch: true do
       get "/manifests?aq[content.generator.name]=company"
       expect(response.body).to include(manifest.tracking_number.to_s)
     end
+
+    it 'paginates' do
+      10.times { create(:manifest, :indexed) }
+      get "/manifests?aq[content.generator.name]=company&page=2&size=2"
+      expect(response.body).to include("Currently viewing 3-4 of 10 results")
+    end
   end
 end
