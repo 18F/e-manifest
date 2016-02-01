0f1bb0d2a5fa8db5b3141692c1e52e4b5b3ae44f
Peter Karman <peter.karman@gsa.gov>
2016-02-01 14:54:53 -0600

    add Authorization Service url/client

--- app/models/cdx/client.rb
+++ app/models/cdx/client.rb
@@ -4,6 +4,7 @@ class CDX::Client
   CDX_BASE_URL = ENV['CDX_BASE_URL'] || "https://devngn.epacdxnode.net"
   CDX_SIGN_SERVICE_URL = ENV['CDX_SIGN_SERVICE_URL'] || "#{CDX_BASE_URL}/cdx-register/services/RegisterSignService?wsdl"
   CDX_AUTH_SERVICE_URL = ENV['CDX_AUTH_SERVICE_URL'] || "#{CDX_BASE_URL}/cdx-register/services/RegisterAuthService?wsdl"
+  CDX_AUTHZ_SERVICE_URL = ENV['CDX_AUTHZ_SERVICE_URL'] || "#{CDX_BASE_URL}/cdx-register/services/RegisterService?wsdl"
 
   def initialize(opts={})
     @savon = Savon.client(default_opts.merge(opts))
@@ -15,7 +16,7 @@ class CDX::Client
       pretty_print_xml: true,
       log: (ENV['CDX_LOG'].present? ? ENV['CDX_LOG'].to_bool : false),
       soap_version: 2,
-      convert_request_keys_to: :none,
+      convert_request_keys_to: :lower_camelcase
     }
   end
 
@@ -35,10 +36,18 @@ class CDX::Client
     })
   end
 
+  def self.authz
+    new({
+      filters: [:password],
+      wsdl: CDX_AUTHZ_SERVICE_URL
+    })
+  end
+
   extend Forwardable
 
   def_delegators :savon, :call, :operations
 
   Signin = signin
   Auth = auth
+  Authz = authz
 end
