5b55850983677537d1f42c846f083365cab6d0f2
Peter Karman <peter.karman@gsa.gov>
2016-02-02 15:12:09 -0600

    cache the security_token between SOAP objects

--- app/models/cdx/profile_request.rb
+++ app/models/cdx/profile_request.rb
@@ -2,7 +2,7 @@ class CDX::ProfileRequest < CDX::LoggedRequest
   private
 
   def security_token
-    @security_token ||= CDX::System.new(output_stream).perform
+    @security_token ||= opts[:security_token] || CDX::System.new(output_stream).perform
   end 
 
   def client
