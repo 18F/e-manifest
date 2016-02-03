03d801b89cf467ec6fb272390029dad1d9421a95
Peter Karman <peter.karman@gsa.gov>
2016-02-03 11:51:05 -0600

    tests for UserOrgRole

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe User do
   describe 'Associations' do
     it { should have_many(:manifests) }
+    it { should have_many(:user_org_roles) }
+    it { should have_many(:roles) }
+    it { should have_many(:organizations) }
   end
 
   describe 'cdx_user_id' do
@@ -22,6 +25,32 @@ describe User do
       user = User.find_or_create(SecureRandom.hex()[0..15])
       user2 = User.find_or_create(user.cdx_user_id)
       expect(user).to eq user2
+    end
+  end
+
+  describe 'user_org_roles' do
+    it '#role_for_org' do
+      user_org_role = create(:user_org_role)
+      user = user_org_role.user
+      organization = user_org_role.organization
+      role = user_org_role.role
+      expect(user.role_for_org(organization.cdx_org_name, role.cdx_role_name)).to eq([user_org_role])
+    end
+
+    it '#has_role_for_org?' do
+      user_org_role = create(:user_org_role)
+      user = user_org_role.user
+      organization = user_org_role.organization
+      role = user_org_role.role
+      expect(user.has_role_for_org?(organization.cdx_org_name, role.cdx_role_name)).to eq(true)
+    end
+
+    it '#org_role_status_for' do
+      user_org_role = create(:user_org_role, cdx_status: 'foo')
+      user = user_org_role.user
+      organization = user_org_role.organization
+      role = user_org_role.role
+      expect(user.org_role_status_for(organization.cdx_org_name, role.cdx_role_name)).to eq('foo')
     end
   end
 end
