c4ac4267db443f934d43ae786ebb5c06e4f0166a
Peter Karman <peter.karman@gsa.gov>
2016-02-03 11:04:44 -0600

    cache CDX responses more aggressively for re-use

--- app/models/cdx/profile_request.rb
+++ app/models/cdx/profile_request.rb
@@ -1,9 +1,9 @@
 class CDX::ProfileRequest < CDX::LoggedRequest
-  private
-
   def security_token
     @security_token ||= opts[:security_token] || CDX::System.new(output_stream).perform
   end 
+
+  private
 
   def client
     CDX::Client::Authz
