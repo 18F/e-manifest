a3f413453811c935bab3a6833c1ad47707011dc8
Peter Karman <peter.karman@gsa.gov>
2016-02-03 11:04:44 -0600

    cache CDX responses more aggressively for re-use

--- app/services/user_profile_builder.rb
+++ app/services/user_profile_builder.rb
@@ -8,11 +8,23 @@ class UserProfileBuilder
 
   def run
     profile = { organizations: {} }
-    profile[:user] = CDX::UserProfile.new(user_id: user.cdx_user_id).perform
-    organizations = CDX::UserOrganizations.new(user_id: user.cdx_user_id, dataflow: dataflow).perform
+    user_profile = CDX::UserProfile.new(user_id: user.cdx_user_id)
+    profile[:user] = user_profile.perform
+    organizations = CDX::UserOrganizations.new(
+      user_id: user.cdx_user_id,
+      dataflow: dataflow,
+      user: profile[:user],
+      security_token: user_profile.security_token
+    ).perform
     organizations.each do |org|
       profile[:organizations][org[:organization_name]] = { org: org, roles: {} }
-      roles = CDX::UserRoles.new(user_id: user.cdx_user_id, dataflow: dataflow, organization: org).perform
+      roles = CDX::UserRoles.new(
+        user_id: user.cdx_user_id,
+        dataflow: dataflow,
+        organization: org,
+        user: profile[:user],
+        security_token: user_profile.security_token
+      ).perform
       roles.each do |role|
         profile[:organizations][org[:organization_name]][:roles][role[:type][:description]] = role
       end
