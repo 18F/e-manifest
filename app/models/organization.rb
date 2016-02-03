5051cf1735002407f9c0b88c2a3bd1454c0a1410
Peter Karman <peter.karman@gsa.gov>
2016-02-03 11:28:18 -0600

    wrap model sync in a single transaction; sync status if CDX changes

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,17 +3,17 @@ class Organization < ActiveRecord::Base
 
   validates :cdx_org_name, :cdx_org_id, presence: true
 
-  def from_cdx(cdx_org)
+  def self.from_cdx(cdx_org)
     find_from_cdx(cdx_org) || create_from_cdx(cdx_org)
   end
 
   private
 
-  def find_from_cdx(cdx_org)
+  def self.find_from_cdx(cdx_org)
     find_by(cdx_org_name: cdx_org[:organization_name], cdx_org_id: cdx_org[:organization_id])
   end
 
-  def create_from_cdx(cdx_org)
+  def self.create_from_cdx(cdx_org)
     create(cdx_org_name: cdx_org[:organization_name], cdx_org_id: cdx_org[:organization_id])
   end
 end
