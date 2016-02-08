9e768ae7a3950b39ca011f9a478d9282da91a7ab
Peter Karman <peter.karman@gsa.gov>
2016-02-08 15:35:17 -0600

    VCR tests for CDX::UserRoles

--- spec/models/cdx/user_organizations_spec.rb
+++ spec/models/cdx/user_organizations_spec.rb
@@ -4,15 +4,9 @@ describe 'CDX::UserOrganizations' do
   it '#perform' do
     VCR.use_cassette('user_organizations_perform') do
       opts = { user_id: 'e-manifest-dev', dataflow: 'eManifest' }
-      orgs = CDX::UserOrganizations.new(opts, output_stream)
+      orgs = CDX::UserOrganizations.new(opts, stream_logger)
       response = orgs.perform
       expect(response[0][:organization_name]).to eq('EPA 2')
     end
-  end
-
-  private
-
-  def output_stream
-    @_output_stream ||= StringIO.new('')
   end
 end
