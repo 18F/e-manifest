18F/e-manifest

commit 38e63f3a943e559990ce6727d8e87f2cf8270bd4

CDX API has inconsistent case conventions: sometimes CamelCase sometimes lowerCamelCase, so do case mangling ourselves

app/models/cdx/profile_request.rb
@@ -16,4 +16,27 @@ class CDX::ProfileRequest < CDX::LoggedRequest
   def client
     CDX::Client::Authz
   end
+
+  def lower_camelize(thing)
+    if thing.is_a?(Hash)
+      lower_camelize_hash(thing)
+    elsif thing.is_a?(Array)
+      thing.each do |subthing|
+        lower_camelize(subthing)
+      end
+    else
+      fail "Unsupported object: #{thing.pretty_inspect}"
+    end
+    thing
+  end
+
+  def lower_camelize_hash(hash)
+    hash.keys.each do |key|
+      new_key = key.to_s.camelize(:lower).to_sym
+      hash[new_key] = hash.delete(key)
+      if hash[new_key].is_a?(Hash) || hash[new_key].is_a?(Array)
+        lower_camelize(hash[new_key])
+      end
+    end
+  end
 end

app/models/cdx/client.rb
@@ -16,7 +16,7 @@ class CDX::Client
       pretty_print_xml: true,
       log: (ENV['CDX_LOG'].present? ? ENV['CDX_LOG'].to_bool : false),
       soap_version: 2,
-      convert_request_keys_to: :lower_camelcase
+      convert_request_keys_to: :none
     }
   end
 

app/models/cdx/user_organizations.rb
@@ -15,7 +15,7 @@ class CDX::UserOrganizations < CDX::ProfileRequest
   end
 
   def repackage_response
-    orgs_data.is_a?(Array) ? orgs_data : [orgs_data]
+    lower_camelize(orgs_data.is_a?(Array) ? orgs_data : [orgs_data])
   end 
 
   def orgs_data

app/models/cdx/user_profile.rb
@@ -14,7 +14,7 @@ class CDX::UserProfile < CDX::ProfileRequest
   end
 
   def repackage_response
-    user_data
+    lower_camelize(user_data)
   end
 
   def user_data

app/models/cdx/user_roles.rb
@@ -16,7 +16,7 @@ class CDX::UserRoles < CDX::ProfileRequest
   end
 
   def repackage_response
-    roles_data.is_a?(Array) ? roles_data : [roles_data]
+    lower_camelize(roles_data.is_a?(Array) ? roles_data : [roles_data])
   end 
 
   def roles_data

spec/models/cdx/user_organizations_spec.rb
@@ -6,7 +6,7 @@ describe 'CDX::UserOrganizations' do
       opts = { user_id: 'e-manifest-dev', dataflow: 'eManifest' }
       orgs = CDX::UserOrganizations.new(opts, stream_logger)
       response = orgs.perform
-      expect(response[0][:organization_name]).to eq('EPA 2')
+      expect(response[0][:organizationName]).to eq('EPA 2')
     end
   end
 end

spec/models/cdx/user_profile_spec.rb
@@ -5,7 +5,7 @@ describe 'CDX::UserProfile' do
     VCR.use_cassette('user_profile_perform') do
       opts = { user_id: 'e-manifest-dev' }
       profile = CDX::UserProfile.new(opts, stream_logger).perform
-      expect(profile[:first_name]).to eq('Test')
+      expect(profile[:firstName]).to eq('Test')
     end
   end
 end

commit ed6a615507e3b60762d5571be039573324915e07

fix attribute case (regression from 38e63f3a943e559990ce6727d8e87f2cf8270bd4); add tests for UserProfileBuilder and UserProfileSyncer, and background worker to build user profile

app/models/cdx/profile_request.rb
@@ -24,6 +24,8 @@ class CDX::ProfileRequest < CDX::LoggedRequest
       thing.each do |subthing|
         lower_camelize(subthing)
       end
+    elsif thing.nil?
+      # no-op
     else
       fail "Unsupported object: #{thing.pretty_inspect}"
     end

app/models/organization.rb
@@ -12,10 +12,10 @@ class Organization < ActiveRecord::Base
   private
 
   def self.find_from_cdx(cdx_org)
-    find_by(cdx_org_name: cdx_org[:organization_name], cdx_org_id: cdx_org[:organization_id])
+    find_by(cdx_org_name: cdx_org[:organizationName], cdx_org_id: cdx_org[:organizationId])
   end
 
   def self.create_from_cdx(cdx_org)
-    create(cdx_org_name: cdx_org[:organization_name], cdx_org_id: cdx_org[:organization_id])
+    create(cdx_org_name: cdx_org[:organizationName], cdx_org_id: cdx_org[:organizationId])
   end
 end

app/services/user_authenticator.rb
@@ -57,12 +57,15 @@ class UserAuthenticator
       nil
     else
       user = User.find_or_create(user_id)
-      UserSession.create(user, cdx_response)
+      session = UserSession.create(user, cdx_response)
+      UserProfileWorker.perform_async(user.id)
+      session
     end
   end
 
   def merge_session(cdx_response)
     @session.user = User.find_or_create(user_id)
+    UserProfileWorker.perform_async(@session.user.id)
     @session.merge_cdx(cdx_response)
   end
 end

app/services/user_profile_builder.rb
@@ -32,7 +32,7 @@ class UserProfileBuilder
 
   def populate_roles(organizations, profile, security_token)
     organizations.each do |org|
-      profile[:organizations][org[:organization_name]] = { org: org, roles: {} }
+      profile[:organizations][org[:organizationName]] = { org: org, roles: {} }
       roles = CDX::UserRoles.new(
         user_id: user.cdx_user_id,
         dataflow: dataflow,
@@ -41,7 +41,7 @@ class UserProfileBuilder
         security_token: security_token
       ).perform
       roles.each do |role|
-        profile[:organizations][org[:organization_name]][:roles][role[:type][:description]] = role
+        profile[:organizations][org[:organizationName]][:roles][role[:type][:description]] = role
       end
     end
   end

app/services/user_profile_syncer.rb
@@ -50,7 +50,7 @@ class UserProfileSyncer
     org = Organization.from_cdx(cdx_org)
     role = Role.from_cdx(cdx_role)
     user_org_role = UserOrgRole.new(organization: org, role: role)
-    user_org_role.cdx_user_role_id = cdx_role[:user_role_id]
+    user_org_role.cdx_user_role_id = cdx_role[:userRoleId]
     user_org_role.cdx_status = cdx_role[:status][:code]
     user.user_org_roles << user_org_role
   end

app/workers/user_profile_worker.rb
@@ -1,0 +1,13 @@
+class UserProfileWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'default', retry: true, backtrace: true
+
+  def perform(user_id, dataflow = ENV['CDX_DEFAULT_DATAFLOW'])
+    user = User.find(user_id)
+    builder = UserProfileBuilder.new(user, dataflow)
+    profile = builder.run
+    puts profile.pretty_inspect
+    syncer = UserProfileSyncer.new(user, profile)
+    syncer.run
+  end
+end

spec/models/organization_spec.rb
@@ -9,7 +9,7 @@ describe Organization do
 
   describe '#from_cdx' do
     it 'parsed CDX response' do
-      cdx_org = { organization_name: 'foo', organization_id: '123' }
+      cdx_org = { organizationName: 'foo', organizationId: '123' }
       organization = Organization.from_cdx(cdx_org)
       expect(organization).to be_a(Organization)
       expect(organization.cdx_org_name).to eq('foo')

spec/services/user_profile_builder_spec.rb
@@ -1,0 +1,10 @@
+require 'rails_helper'
+
+describe UserProfileBuilder do
+  it 'builds a profile' do
+    mock_cdx_profiles
+    user = create(:user, cdx_user_id: 'some_user')
+    builder = UserProfileBuilder.new(user, 'foo')
+    expect(builder.run).to eq(mock_cdx_user_profile)
+  end
+end

spec/services/user_profile_syncer_spec.rb
@@ -1,0 +1,11 @@
+require 'rails_helper'
+
+describe UserProfileSyncer do
+  it 'builds orgs + roles from a profile' do
+    user = create(:user, cdx_user_id: 'some_user')
+    syncer = UserProfileSyncer.new(user, mock_cdx_user_profile)
+    syncer.run
+    expect(user.organizations.map(&:cdx_org_name)).to eq(['EPA 2'])
+    expect(user.roles.map(&:cdx_role_name)).to eq(['TSDF'])
+  end
+end

spec/support/user_authenticator_helper.rb
@@ -25,4 +25,45 @@ module UserAuthenticatorHelper
     allow_any_instance_of(UserAuthenticator).to receive(:authorize_signature).and_return(nil)
     allow_any_instance_of(UserAuthenticator).to receive(:error_message).and_return('Bad user_id or password')
   end
+
+  def mock_cdx_user_profile
+    {
+      organizations: {
+        "EPA 2" => {
+          org: {
+            organizationId: "15404",
+            organizationName: "EPA 2",
+            primaryOrg: true,
+            userOrganizationId: "86328",
+          },
+          roles: {
+            "TSDF" => {
+              dataflow: "eManifest",
+              status: {
+                code: "Active",
+                description: "Active"
+              },
+              type: {
+                code: "112090",
+                description: "TSDF",
+                status: "Active"
+              },
+              userRoleId: "87638"
+            }
+          }
+        }
+      },
+      user: {
+        firstName: "Test",
+        lastName: "Tester",
+        status: "Valid",
+        userId: "some_user",
+      }
+    }
+  end
+
+  def mock_cdx_profiles
+    allow(UserProfileWorker).to receive(:perform_async).and_return(true)
+    allow_any_instance_of(UserProfileBuilder).to receive(:run).and_return(mock_cdx_user_profile)
+  end
 end