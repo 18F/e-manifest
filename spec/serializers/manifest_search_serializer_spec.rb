9334b934c903c2d977fadc9bedf9b4579824fe7f
Peter Karman <peter.karman@gsa.gov>
2016-02-16 09:49:17 -0600

    fix var scope regression

--- spec/serializers/manifest_search_serializer_spec.rb
+++ spec/serializers/manifest_search_serializer_spec.rb
@@ -7,11 +7,11 @@ describe ManifestSearchSerializer do
     manifest.reload
     es_execute_with_retries 3 do
       es_response = Manifest.authorized_search({q: manifest.uuid})[:es_response]
-    end
-    serialized = ManifestSearchSerializer.new(es_response).to_json
+      serialized = ManifestSearchSerializer.new(es_response).to_json
 
-    serialized_from_json = JSON.parse(serialized)
-    expect(serialized_from_json['total']).to eq 1
-    expect(serialized_from_json['hits'][0]['id']).to eq(manifest.uuid)
+      serialized_from_json = JSON.parse(serialized)
+      expect(serialized_from_json['total']).to eq 1
+      expect(serialized_from_json['hits'][0]['id']).to eq(manifest.uuid)
+    end
   end
 end
