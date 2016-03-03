3f117601d5e6118c6bd42fe4e85885b2024c6f4b
Peter Karman <peter.karman@gsa.gov>
2016-03-03 17:08:19 -0600

    use hard string from old cassette

--- spec/models/cdx/user_roles_spec.rb
+++ spec/models/cdx/user_roles_spec.rb
@@ -7,7 +7,7 @@ describe 'CDX::UserRoles' do
       orgs = CDX::UserOrganizations.new(opts, stream_logger).perform
       orgs.each do |org|
         roles = CDX::UserRoles.new(opts.merge(organization: org), stream_logger).perform
-        expect(roles[0][:type][:description]).to eq('TSDF Certifier')
+        expect(roles[0][:type][:description]).to eq('TSDF') # old VCR cassette has old name
       end
     end
   end
